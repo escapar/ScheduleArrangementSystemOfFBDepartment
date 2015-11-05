8ce4fb85df34c2d34353394461eaf60ea1511212
ThrowableFactory <ThrowableFactory@foxmail.com>
2015-11-05 13:01:40 +0800

    部署

--- src/main/java/cn/edu/shnu/fb/ScheduleArrangementApplication.java
+++ src/main/java/cn/edu/shnu/fb/ScheduleArrangementApplication.java
@@ -1,18 +1,23 @@
 package cn.edu.shnu.fb;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
-public class ScheduleArrangementApplication {
+public class ScheduleArrangementApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ScheduleArrangementApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ScheduleArrangementApplication.class);
     }
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ScheduleArrangementApplication.class, args);
+    }
 
     @Bean
     public CharacterEncodingFilter characterEncodingFilter() {
